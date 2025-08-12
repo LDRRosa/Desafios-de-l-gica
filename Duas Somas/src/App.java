3c430f65fa63edfdfa54219b0d890ba2f7fb8eb7
Leandro Rosa <137298525+LDRRosa@users.noreply.github.com>
2025-08-12 17:01:30 -0300

    Add files via upload

---
+++ Duas Somas/src/App.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] vet = { 3, 2, 4, 7, 5, 20, 40, 3, 75, 1, 2, 15, 10, 9 };
+        int alvo = 10;
+
+        for (int i = 0; i < vet.length; i++) {
+            int atual = vet[i];
+            boolean encontrado = false;
+            for (int j = 0; j < vet.length; j++) {
+                if (atual + vet[j] == alvo) {
+                    System.out.println("posições " + i + " " + j);
+                    encontrado = true;
+                    break;
+                }
+            }
+            if(encontrado==true){
+                break;
+            }
+
+        }
+
+    }
+}
